5863acaf33c4a10b2736c45deb1a50e15fbbe276
Emmanuelludo <96787156+Emmanuelludo@users.noreply.github.com>
2026-08-19 10:12:28 +0200

    Remove interview presentation source from public repository

--- presentation/build_deck.js
+++
@@ -1,6 +0,0 @@
-const ctx = require('./deck_context');
-require('./slides_01_06')(ctx);
-require('./slides_07_13')(ctx);
-
-const out = ctx.path.join(ctx.ROOT, 'presentation/FamilyPRS_Lab_v6_QR_Interview_Presentation.pptx');
-ctx.pptx.writeFile({ fileName: out });
